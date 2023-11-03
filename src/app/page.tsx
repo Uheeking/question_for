eda7fad1eeeef7a723539a6ffb81ec4fc2a36f45
Uheeking <wert1847@naver.com>
2023-11-03 16:44:29 +0900

    MODIFY : 메인 페이지 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image'
+import profile from "../../public/profile.png";
 
 export default function Home() {
   return (
     <div>
       <div className='font-bold text-4xl text-brown-900 dark:text-slate-300 tracking-tighter'>Uheeking</div>
-      {/* <div className='object-cover inset-0 rounded-full h-[114px] w-[114px]'></div> */}
+      <div className='flex w-full justify-center'>
+        <img src="/profile.png" className='bg-white object-cover inset-0 rounded-full h-[114px] w-[114px]'/>
+        <div>My name is :: <span className='bg-black text-white'>Uheeking</span></div>
+      </div>
     <aside className="bg-black text-white p-6 rounded-lg w-full max-w-lg font-mono">
       <div className="flex justify-between items-center">
         <div className="flex space-x-2 text-red-500">
